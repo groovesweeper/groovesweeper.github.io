8197afbe6f67598f367c85d89142462d1cfeccfa
JamesY <youngj15>
2021-03-21 19:05:36 -0400

    client details in gitignore and way to extrapolate api details from text file

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
+import lyricsgenius
+
 if __name__ == "__main__":
-    print("Welcome To GrooveSweeper!")
+    #print("Welcome To GrooveSweeper!")
+    '''
+    Dict holds exactly
+    CLIENT_ID:<ID from genius API>
+    CLIENT_SECRET:<secret from Genius API>
+    CLIENT_TOKEN:<token from Genius API>
+    '''
+	
+	client_details = {}
+	with open("client_details.txt") as f:
+		for line in f:
+			(key, val) = line.split(":")
+			client_details[key] = val
+
+	for key in client_details:
+		print(key, ":", client_details[key])
