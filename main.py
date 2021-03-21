6e832ec56c4e3709409da2c40cd679a044c1ae63
Jane <liz27@rpi.edu>
2021-03-21 19:15:28 -0400

    Main reads in tokens

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ if __name__ == "__main__":
     CLIENT_SECRET:<secret from Genius API>
     CLIENT_TOKEN:<token from Genius API>
     '''
-	
-	client_details = {}
-	with open("client_details.txt") as f:
-		for line in f:
-			(key, val) = line.split(":")
-			client_details[key] = val
 
-	for key in client_details:
-		print(key, ":", client_details[key])
+    client_details = {}
+
+    with open("client_details.txt") as f:
+        for line in f:
+            (key, val) = line.split(":")
+            client_details[key] = val
+
+    genius = lyricsgenius.Genius(client_details["CLIENT_TOKEN"])
