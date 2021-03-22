68b8d1ad6e9793a6d28474395b56affcc128fac2
Jane <liz27@rpi.edu>
2021-03-21 20:01:16 -0400

    Added the django project and created a classes folder and a Filter class

---
+++ Filter.py
@@ -0,0 +1,34 @@
+class Filter:
+	SEVEN_DIRTY_WORDS = ("fuck", "shit", "piss", "cunt", "cocksucker", "motherfucker", "tits")
+	COMMON_SWEAR_WORDS = ("ass", "dick", "pussy", "bitch")
+	__customFilterWords = set()
+
+	# fullFilter is all current filtered words
+	__fullFilter = set()
+	__instance = None
+
+	#
+
+	def __init__(self):
+		if Filter.__instance != None:
+			raise Exception("This class is a singleton! Call getInstance() instead.")
+		else:
+			for word in SEVEN_DIRTY_WORDS:
+				Filter.__fullFilter.add(word)
+			for word in COMMON_SWEAR_WORDS:
+				Filter.__fullFilter.add(word)
+			Filter.__instance = self
+
+	@staticmethod
+	def getInstance():
+		if Filter.__instance == None:
+			Filter()
+		return Filter.__instance
+
+	def addWord(word):
+		# TODO Implement
+		return False
+
+	def removeWord(word):
+		# TODO Implement
+		return False
