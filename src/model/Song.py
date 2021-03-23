837e1cd1789981057583aa30b2e60dbb38b5b808
Phillip <pesmithiii7@gmail.com>
2021-03-23 01:37:43 -0500

    Setup Song class for development

---
+++ src/model/Song.py
@@ -0,0 +1,50 @@
+from __init__ import *
+from Filter import *
+from main import *
+
+
+class Song:
+    # Name of the song query as a string
+    __name = str()
+    # Name of the artist query as a string
+    __artist = str()
+    # List of all strings in a song
+    __lyrics = list()
+    # Set of all filtered explicit words in the song
+    __explicitWords = set()
+    # URL of the song?
+    __url = str()
+
+    def __init__(self):
+        # TODO something
+        return
+
+    @staticmethod
+    def __parseLyrics(self, __lyrics):
+        # TODO something
+        return
+
+    def getName(self):
+        # TODO something
+        return
+
+    def getArtist(self):
+        # TODO something
+        return
+
+    def ExplicitWords(self):
+        # TODO something
+        return
+
+    def getNumOfExplicitWords(self):
+        # TODO something
+        return
+
+    def isExplicit(self):
+        # TODO something
+        return
+
+    def filterSong(self, _instance):
+        # TODO something
+        return
+
