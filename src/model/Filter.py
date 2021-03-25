f5086bf25450e90bea53e5a35a73a32fd75f9a71
JamesY <youngj15>
2021-03-25 02:20:00 -0400

    groundwork for basic command loop and filter editing

--- src/model/Filter.py
+++ src/model/Filter.py
@@ -13,9 +13,9 @@ class Filter:
         if Filter.__instance is not None:
             raise Exception("This class is a singleton! Call getInstance() instead.")
         else:
-            for word in SEVEN_DIRTY_WORDS:
+            for word in self.SEVEN_DIRTY_WORDS:
                 Filter.__fullFilter.add(word)
-            for word in COMMON_SWEAR_WORDS:
+            for word in self.COMMON_SWEAR_WORDS:
                 Filter.__fullFilter.add(word)
             Filter.__instance = self
 
@@ -30,6 +30,13 @@ class Filter:
         return False
 
     def removeWord(word):
+        # TODO Implement
+        return
+
+    def filteredWords():
+        # I figured this would be a useful function to print out all
+        # words in __fullFilter
+
         # TODO Implement
         return
 
