fbdd311ee2a85bf293ae37cccad236260e7348cf
Phillip <pesmithiii7@gmail.com>
2021-03-25 15:36:43 -0500

    Removed client_details and lyricsgenius import. Edited methods.

--- src/model/Song.py
+++ src/model/Song.py
@@ -1,6 +1,8 @@
 from __init__ import *
 from Filter import *
 from main import *
+from Playlist import *
+import sys
 
 
 class Song:
@@ -15,35 +17,83 @@ class Song:
     # URL of the song?
     __url = str()
 
-    def __init__(self):
-        # TODO something
-        return
+    """
+    If the song or artist lists are not empty a query is initiated.
+    If the song or artist lists are empty, the user is asked to query.
+    If an artist is queried, user chooses a song from the list to query.
+    """
 
+    def __init__(self, lyrics, artist, name, url):
+        self.__lyrics = lyrics
+        self.__artist = artist
+        self.__name = name
+        self.__url = url
+
+    # file of selected song is returned as a list of strings
     @staticmethod
     def __parseLyrics(self, __lyrics):
-        # TODO something
-        return
+        """
+        original_stdout = sys.stdout
+        with open('lyrics.txt', 'w') as f:
+            sys.stdout = f
+            print(genius.lyrics("happy", "pharrell"))
+            sys.stdout = original_stdout
+        # get full output of lyrics???
+        # for line in file:
+        # eliminate punctuation
+        # parse by space
+        # append words to a list
+        """
+        return self.__lyrics
 
+    @staticmethod
+    # asks user to input a song name
     def getName(self):
-        # TODO something
-        return
+        """
+        if Song.__name is None:
+            song_name = input("Enter a song name: ").strip()
+            Song.__name.append(song_name)
+        else:
+            Song()
+        """
+        return self.__name
 
+    @staticmethod
+    # asks user to input an artist name
     def getArtist(self):
-        # TODO something
-        return
+        """
+        if Song.__artist is None:
+            artist_name = input("Enter an artist name: ").strip()
+            Song.__artist.append(artist_name)
+        else:
+            Song()
+        """
+        return self.__artist
 
-    def ExplicitWords(self):
-        # TODO something
-        return
+    # Finds explicit words in "__parseLyrics" based on Filter class; adds to __explicitWords
+    def ExplicitWords(self, ):
+        """
+        for words in self.__lyrics:
 
+        return self.__explicitWords
+        """
+
+    # Returns count of _explicitWords
     def getNumOfExplicitWords(self):
-        # TODO something
-        return
+        num = 0
+        num = len(self.__explicitWords)
+        return num
 
+    # Returns True if _explicitWords set is not empty; else False
     def isExplicit(self):
-        # TODO something
+        """
+        explicit = False
+        for song in self.
+            if
+        """
         return
 
+    # Returns instance of filtered word if found in __parseLyrics
     def filterSong(self, _instance):
         # TODO something
         return
