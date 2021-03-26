2301da28214756add8c5b4a0767e03659d62ede1
JackYannes56 <jackyannes29@gmail.com>
2021-03-26 09:09:44 -0400

    Revert "Revert "Merge branch 'main' of https://github.com/groovesweeper/groovesweeper.github.io into main""
    This reverts commit b27b21fc427707f29b2ceb16f5d45e5efd112d7c.

--- src/model/Song.py
+++ src/model/Song.py
@@ -1,100 +1,138 @@
-from __init__ import *
-from Filter import *
-from main import *
-from Playlist import *
-import sys
-
+from Filter import Filter
 
 class Song:
-    # Name of the song query as a string
+    """
+    A class used to determine whether or not a song is explicit or not
+
+    ...
+
+    Attributes
+    ----------
+    __name : string
+        Name of the song being checked for explicit words
+    __artist : string
+        Songs from this artist are checked for explicit words
+    __lyrics : list of strings
+        Contains all words in the song lyrics
+    __explicitWords : set
+        Contains one of each explicit word found in song lyrics
+    __url : string
+        Link to the song lyrics on Genius website
+
+    Methods
+    -------
+    getName() :
+        Returns song name
+    getArtist() :
+        Returns artist name
+    getExplicitWords() :
+        Gets the explicit words in the song.
+    getNumOfExplicitWords() :
+        Returns the number of explicit words in the song.
+    isExplicit() :
+        If __explicitWords contains words, returns True indicating the song is
+		not clean. Otherwise, false.
+    filterSong() :
+        Filters a song for explicit words and adds those words to __explicitWords.
+		When using this function, you can assume that it is the first time
+		filtering the song and explicitWords set will be empty.
+
+
+    """
     __name = str()
-    # Name of the artist query as a string
     __artist = str()
-    # List of all strings in a song
     __lyrics = list()
-    # Set of all filtered explicit words in the song
     __explicitWords = set()
-    # URL of the song?
     __url = str()
 
-    """
-    If the song or artist lists are not empty a query is initiated.
-    If the song or artist lists are empty, the user is asked to query.
-    If an artist is queried, user chooses a song from the list to query.
-    """
-
     def __init__(self, lyrics, artist, name, url):
+        """
+        Initializes variables used in following methods
+        """
         self.__lyrics = lyrics
         self.__artist = artist
         self.__name = name
         self.__url = url
+        self.filterSong()
 
-    # file of selected song is returned as a list of strings
-    @staticmethod
-    def __parseLyrics(self, __lyrics):
-        """
-        original_stdout = sys.stdout
-        with open('lyrics.txt', 'w') as f:
-            sys.stdout = f
-            print(genius.lyrics("happy", "pharrell"))
-            sys.stdout = original_stdout
-        # get full output of lyrics???
-        # for line in file:
-        # eliminate punctuation
-        # parse by space
-        # append words to a list
-        """
-        return self.__lyrics
-
-    @staticmethod
-    # asks user to input a song name
     def getName(self):
         """
-        if Song.__name is None:
-            song_name = input("Enter a song name: ").strip()
-            Song.__name.append(song_name)
-        else:
-            Song()
+        Returns
+        -------
+            Name of song.
         """
         return self.__name
 
-    @staticmethod
-    # asks user to input an artist name
     def getArtist(self):
         """
-        if Song.__artist is None:
-            artist_name = input("Enter an artist name: ").strip()
-            Song.__artist.append(artist_name)
-        else:
-            Song()
+        Returns
+        -------
+            Name of artist.
         """
         return self.__artist
 
-    # Finds explicit words in "__parseLyrics" based on Filter class; adds to __explicitWords
-    def ExplicitWords(self, ):
+    def getExplicitWords(self):
         """
-        for words in self.__lyrics:
+        Gets the set of explicit words in the song.
 
-        return self.__explicitWords
+        Returns
+        -------
+        set
+			A deep copy of the __explicitWords set
         """
+        explicit = set()
+        for word in self.__explicitWords:
+            explicit.add(word)
 
-    # Returns count of _explicitWords
+        return explicit
+
     def getNumOfExplicitWords(self):
-        num = 0
-        num = len(self.__explicitWords)
-        return num
+        """
+		Gets the number of explicit words in the song.
 
-    # Returns True if _explicitWords set is not empty; else False
+        Returns
+        -------
+        int
+            Number of words in __explicitWords
+        """
+        return len(self.__explicitWords)
+
     def isExplicit(self):
         """
-        explicit = False
-        for song in self.
-            if
+        If __explicitWords is not empty, the song instance is considered explicit.
+        Returns
+        -------
+        boolean
+            True if __explicitWords is not empty.
+            False, otherwise.
         """
+        if len(self.__explicitWords) > 0:
+            return True
+        return False
+
+    def filterSong(self):
+        """
+        Filters a song for explicit words and adds those words to __explicitWords.
+		When using this function, you can assume that it is the first time
+		filtering the song and explicitWords set will be empty.
+        Returns
+        -------
+            None
+        """
+		self.__explicitWords = set()
+        filter = Filter.getInstance()
+        fullFilter = filter.getFullFilter()
+        for word in self.__lyrics:
+            if word in fullFilter:
+                self.__explicitWords.add(word)
         return
 
-    # Returns instance of filtered word if found in __parseLyrics
-    def filterSong(self, _instance):
-        # TODO something
-        return
-
+if __name__ == '__main__':
+	# testing to see there is no rep exposure
+	song = Song(["ass", "piss", "Hello"], "Me", "test", "www.test.com")
+	print(song.isExplicit())
+	explicitWords = song.getExplicitWords()
+	print(explicitWords)
+	explicitWords.remove("piss")
+	explicitWords2 = song.getExplicitWords()
+	print(explicitWords2)
