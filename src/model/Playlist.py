fde0f611212fa949456939bb2a2101efec4b0f34
JackYannes56 <jackyannes29@gmail.com>
2021-03-26 09:43:46 -0400

    Updated Playlist to contain correct comments. Also tweaked getCleanPlaylist

--- src/model/Playlist.py
+++ src/model/Playlist.py
@@ -6,23 +6,64 @@ import copy
 
 
 class Playlist:
-    # List of all songs in a playlist
+	"""
+	A class used to represent playlists holding multiple songs
+	...
+
+	Attributes
+	----------
+	_songList: List
+		List of all songs in a playlist
+	_name: String
+		Name of the playlist
+	_numOfExplicit
+		Number of explicit songs in the playlist
+
+	Methods
+	-------
+	getSongs() :
+		returns the list of songs in the current playlist
+	isExplicit() :
+		returns true if any song in the playlist is explicit, returns false otherwise. Also increments
+		_numOfExplicit.
+	getCleanPlaylist() :
+		creates a copy of the current playlist and removes all explicit songs. Returns that new clean playlist.
+	reFilterSongs() :
+		re filters all songs in the current playlist by calling filterSong in Song class
+
+	"""
+    
     _songList = list()
-    # Name of the playlist
     _name = ""
-    # Number of explicit songs in the playlist
     _numOfExplicit = 0
 
     def __init__(self, songList, name):
+    	"""
+		Playlist constructor:
+			Initializes the playlist object and populates _songList and _name
+    	"""
         self._songList = songList
         self._name = name
 
     def getSongs(self):
-        # returns the list of songs in the current playlist
+    	"""
+		returns the list of songs in the current playlist
+		Returns
+		-------
+		list
+			_songList
+    	"""
         return self._songList
 
     def isExplicit(self):
-        # returns true if any song in the playlist is explicit, returns false otherwise
+    	"""
+		returns true if any song in the playlist is explicit, returns false otherwise. Also increments
+		_numOfExplicit.
+		Returns
+		-------
+		boolean
+			True if any song is explicit, false otherwise.
+    	"""
         explicit = False
         for song in self.getSongs():
             if song.isExplicit():
@@ -31,15 +72,29 @@ class Playlist:
         return explicit
 
     def getCleanPlaylist(self):
+    	"""
+		creates a copy of the current playlist and removes all explicit songs. Returns that new clean playlist.
+		Returns
+		-------
+		Playlist object
+			returns the copy of the current playlist, excluding any 
+    	"""
         # creates a copy of the current playlist and removes all explicit songs. Returns that new clean playlist.
         cleanPlaylist = copy.deepcopy(self)
         cleanSongList = cleanPlaylist.getSongs()
+        anyExplicit = False 
         for song in cleanSongList:
             if song.isExplicit():
                 cleanSongList.remove(song)
-        return cleanPlaylist
+                anyExplicit = True
+        if anyExplicit:
+        	return cleanPlaylist
+        else:
+        	return self
 
     def reFilterSongs(self):
-        # re filters all songs in the current playlist by calling filterSong in Song class
+    	"""
+		re filters all songs in the current playlist by calling filterSong in Song class
+    	"""
         for song in self.getSongs():
             Song.Song.filterSong(Filter.getInstance())
