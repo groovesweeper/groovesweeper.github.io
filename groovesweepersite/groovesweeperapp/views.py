aca53cd6f03e79eb934e7ddb6815e7c74c045c93
Jane <liz27@rpi.edu>
2021-04-13 17:30:31 -0400

    Added groovesweeperapp to the installed apps list in settings.py. Was able to get welcome to groovesweeper to show.

--- groovesweepersite/groovesweeperapp/views.py
+++ groovesweepersite/groovesweeperapp/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-	return render(request, "groovesweeperapp/index.html")
+	#return HttpResponse("welcome")
+	return render(request, 'groovesweeperapp/index.html')
