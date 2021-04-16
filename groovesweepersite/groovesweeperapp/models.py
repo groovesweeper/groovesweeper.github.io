21dd360693a0de57464d5e1ce7420e4f5de7bce2
JamesY <youngj15>
2021-04-16 18:25:23 -0400

    empty modal present, committing so I can pull from repo

--- groovesweepersite/groovesweeperapp/models.py
+++ groovesweepersite/groovesweeperapp/models.py
@@ -2,11 +2,13 @@ from django.db import models
 
 # Create your models here.
 class SongModel(models.Model):
+    # Song Model stores information used to create a song object in the database
     name = models.CharField(max_length=50)
     artist = models.CharField(max_length=50)
-    lyrics = models.TextField(max_length=2500)
-    explicit_words = models.TextField(max_length=150)
+    lyrics = models.TextField()
+    explicit_words = models.TextField()
     url = models.CharField(max_length=100)
 
 class SongQueryModel(models.Model):
+    # This exists so we can extract the term from the Model Form in the home view
     term = models.CharField(max_length=100)
--- groovesweepersite/groovesweeperapp/views.py
+++ groovesweepersite/groovesweeperapp/views.py
@@ -3,18 +3,25 @@ from django.http import HttpResponse,HttpResponseRedirect
 from django.urls import reverse
 from .forms import SongQueryForm
 from .models import SongQueryModel
+from .src.model.Filter import Filter
 
 # Create your views here.
 def homeView(request):
+	# Home view when you first load the app. This page has a search bar and edit
+	# filter abilities
 	form = SongQueryForm()
+	filter = Filter.getInstance()
 
+	# This if statement is triggered if a button is pressed on the page
 	if (request.method == 'POST'):
-		form = SongQueryForm(request.POST)
-		if form.is_valid():
-			form.save()
-			query = form.cleaned_data['term']
-			return HttpResponseRedirect(reverse('results', args=(query,)))
-	context = {'form':form}
+		# This if statement is if the pressed button is the search button
+		if 'search-button' in request.POST:
+			form = SongQueryForm(request.POST)
+			if form.is_valid():
+				form.save()
+				query = form.cleaned_data['term']
+				return HttpResponseRedirect(reverse('results', args=(query,)))
+	context = {'form':form, '7dw':str(filter.getSevenDirtyWords())}
 	return render(request, 'groovesweeperapp/index.html', context)
 
 def lyricsView(request, song_id):
