35149fb73f5a0500630590aeb8212f4f59478be9
JamesY <youngj15>
2021-04-22 18:19:23 -0400

    working .exe?

--- groovesweepersite/run.py
+++ groovesweepersite/run.py
@@ -9,16 +9,16 @@ import webbrowser
 def firstTimeSetup():
     print("Doing first time setup\n.\n.\n.")
     f = open("./first_time.txt", "w")
-    try:
+    '''try:
         import django
     except:
-        print("Django not installed-- Installing Now\n.\n.\n.")
-        os.system("pip install django")
-    try:
+        print("Django not installed-- Installing Now\n.\n.\n.")'''
+    os.system("pip install django")
+    '''try:
         import lyricsgenius
     except:
-        print("Lyricsgenius not installed-- Installing Now\n.\n.\n.")
-        os.system("pip install lyricsgenius")
+        print("Lyricsgenius not installed-- Installing Now\n.\n.\n.")'''
+    os.system("pip install lyricsgenius")
 
 
 
@@ -28,4 +28,4 @@ if __name__ == "__main__":
         firstTimeSetup()
     webbrowser.open_new("http://localhost:8000")
     #os.system("python3 ./manage.py flush")
-    os.system("python3 ./manage.py runserver")
+    os.system("python ./manage.py runserver")
